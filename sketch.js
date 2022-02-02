dcd926c4637bc2c99634a68712bb1c2f2d2e1893
Kimberly Arcena <kvarcena@up.edu.ph>
2022-02-02 23:59:42 +0800

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,79 @@
+function setup()
+{
+	//create a large square canvas
+	createCanvas(1500, 1500);
+}
+
+function draw()
+{
+
+	//set the fill colour to red
+	fill(224, 42, 43);
+
+	//set a thick stroke weight for the black lines
+	strokeWeight(12);
+
+	//draw the red rectangle 
+	rect(160, 0, 470, 462);
+    
+    //set the fill colour to white 1
+	fill(231,228,225);
+
+	//set a thick stroke weight for the black lines
+	strokeWeight(15);
+
+	//draw the white rectangle 
+	rect(5, 0, 150, 200);
+    
+     //set the fill colour to white 2
+	fill(231,228,225);
+
+	//set a thick stroke weight for the black lines
+	strokeWeight(12);
+
+	//draw the white rectangle 
+	rect(5, 213, 150, 250);
+    
+    
+     //set the fill colour to blue
+	fill(3,90,157);
+
+	//set a thick stroke weight for the black lines
+	strokeWeight(12);
+
+	//draw the white rectangle 
+	rect(5, 469, 150, 150);
+    
+    
+    
+    //set the fill colour to white 3
+	fill(231,228,225);
+
+	//set a thick stroke weight for the black lines
+	strokeWeight(12);
+
+	//draw the red rectangle 
+	rect(160, 468, 398, 151);
+    
+    
+    //set the fill colour to white 3
+	fill(231,228,225);
+
+	//set a thick stroke weight for the black lines
+	strokeWeight(12);
+
+	//draw the red rectangle 
+	rect(560, 469, 70, 74);
+    
+    
+    
+    //set the fill colour to yellow
+	fill(240,221,110);
+
+	//set a thick stroke weight for the black lines
+	strokeWeight(12);
+
+	//draw the red rectangle 
+	rect(560, 545, 70, 74);
+    
+}
